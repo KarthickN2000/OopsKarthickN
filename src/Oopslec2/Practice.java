d4e1629994089247829f0bab020b18d7b7bde5d4
Karthick N <75425806+KarthickN2000@users.noreply.github.com>
2024-11-21 19:27:51 +0530

    Update Practice.java

--- src/Oopslec2/Practice.java
+++ src/Oopslec2/Practice.java
@@ -3,5 +3,8 @@ package Oopslec2;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("This file is for practice");
+        int a = 10;
+        int b = 15;
+        System.out.println(a + b);
     }
 }
