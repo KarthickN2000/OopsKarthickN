3b5efe16c1958f822a8de2804dd9fb201cd29d3c
KarthickN2000 <karthiknellai12345@gmail.com>
2024-11-27 15:33:47 +0530

    Committed changes

--- src/Oopslec2/Protected.java
+++ src/Oopslec2/Protected.java
@@ -10,19 +10,25 @@ public class Protected extends Box {
 
     @Override
     public boolean equals(Object obj) {
-        return this.l ==  ((Protected)obj).l;
+        return this.l == ((Protected)obj).l;
+    }
+
+    @Override
+    public String toString() {
+        return "Protected{" +
+                "l=" + this.l +
+                '}';
     }
 
     public static void main(String[] args) {
         Box obj = new Protected(1,2,3);
         Box obj2 = new Protected(1,2,3);
-        if(obj == obj2){
+        /*if(obj == obj2){
             System.out.println("Both the objects are equal.");
-        }
-        /*if(obj.equals(obj2)){
-            System.out.println("Both the objects are equal.");
-        }
-*/
+        }*/
+
+//        if we want some info about package and classes we can use .getClass() method present in object class
+        System.out.println(obj.getClass());
 //        System.out.println(obj.l); this provide error because l variable is protected in Box class
 //        so it can be accessed only via subclass as reference variable in diff package
     }
